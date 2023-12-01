cb79c62958d01577a1733e92029bfeea525c1dc9
Karen Lyon <144794853+Karelyon@users.noreply.github.com>
2023-12-01 15:53:17 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let pronouns = ["the", "our", "an", "some"];
+let adjs = ["great", "big", "small", "beauty"];
+let nouns = ["jogger", "racoon", "mouse", "queen"];
+let exts = [".com", ".es", ".us", ".net"];
+
+for (let pronoun of pronouns) {
+  for (let adj of adjs) {
+    for (let noun of nouns) {
+      for (let ext of exts) {
+        console.log(`${pronoun}${adj}${noun}${ext}`);
+      }
+    }
+  }
+}
